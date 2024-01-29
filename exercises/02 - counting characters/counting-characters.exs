70955f1d2914585e72057012b51ffae29369e5e9
Alexander Wilkinson <alex.wilkinson@impala.travel>
2024-01-29 20:12:13 +0000

    feat: added in basics for some small elixir test projects

---
+++ exercises/02 - counting characters/counting-characters.exs
@@ -0,0 +1,13 @@
+/**
+ * EXERCISE 2
+ * Program that accepts a string and counts how long it is
+ *
+ * example:
+ * What is the input string? Homer
+ * Homer has 5 characters
+ *
+ * Additional
+ *  - if empty string prompt and allow them to re-enter
+ *  - output contains original string
+ *  - output is single string
+ */
